1b19450afe7c265221d92227ccdd0f59d348945a
sasyuke <rassulmagauin@gmail.com>
2025-03-10 23:35:29 +0500

    initial push

---
+++ src/main/java/com/example/smartrecruiters_backend/SmartRecruitersBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartrecruiters_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRecruitersBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartRecruitersBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smartrecruiters_backend/SmartRecruitersBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartrecruiters_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRecruitersBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
